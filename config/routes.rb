8bc64344c93748d9423accc5f8665fdd9926b5f5
Johnathan Zamora <Zoltano@mactop.mobile.asu.edu>
2017-04-01 19:23:52 -0700

    Updated routes to all GET

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get '/return_all'	=> 'memes#return_all'
 
-  post '/save_meme' => 'memes#save_meme'
+  get '/save_meme' => 'memes#save_meme'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
